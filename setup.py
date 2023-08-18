5f84a5d4a068ee9d902fddd3749be64797ae791e
ezequielpf <ezequielpf@gmail.com>
2023-08-18 16:25:34 -0300

    Project started

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='The project aims to order potential clients',
+    author='Ezequiel Pelisoli Francisco',
+    license='MIT',
+)
